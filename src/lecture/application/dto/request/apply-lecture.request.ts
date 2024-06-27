a84989cff2257da757d108a4ae2bd93c4beb0d9f
DongGunYoon <donggun961108@gmail.com>
2024-06-27 18:32:37 +0900

    test: 강의 수강 신청 관련 통합 테스트 추가

--- src/lecture/application/dto/request/apply-lecture.request.ts
+++ src/lecture/application/dto/request/apply-lecture.request.ts
@@ -2,6 +2,11 @@ import { IsInt } from 'class-validator';
 import { CreateLectureApplicationDTO } from 'src/lecture/domain/dto/create-lecture-application.dto';
 
 export class ApplyLectureRequest {
+  constructor(userId: number, lectureScheduleId: number) {
+    this.userId = userId;
+    this.lectureScheduleId = lectureScheduleId;
+  }
+
   @IsInt()
   userId: number;
 
